ec6fe257ac5efe3f8b29715e9ec72026a5876a02
AlienBlox <alienthebest123@outlook.com>
2026-05-26 11:13:44 +0100

    take a copy

---
+++ cogs/TakeACopy.py
@@ -0,0 +1,14 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class Utility(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="takeme", description="Take a copy of the Ogre Bot")
+    async def ping(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f'Take a copy of me at https://github.com/AlienBlox/Ogre! \n Install at https://discord.com/oauth2/authorize?client_id=1508446745816989796&permissions=8&integration_type=0&scope=bot+applications.commands')
+
+async def setup(bot):
+    await bot.add_cog(Utility(bot))
