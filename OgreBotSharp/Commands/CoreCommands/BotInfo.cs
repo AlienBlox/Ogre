65a8a662d85fb67a1f3e579233d1c9d5573a08b5
AlienBlox <alienthebest123@outlook.com>
2026-05-27 11:12:43 +0100

    retry

--- OgreBotSharp/Commands/CoreCommands/BotInfo.cs
+++ OgreBotSharp/Commands/CoreCommands/BotInfo.cs
@@ -2,31 +2,36 @@
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 
-namespace OgreBotSharp.Commands.CoreCommands
+public class BotInfo : ApplicationCommandModule
 {
-    public class BotInfo : ApplicationCommandModule
+    [SlashCommand("botinfo", "Get information about the bot.")]
+    public async Task BotInfoCommand(InteractionContext ctx)
     {
-        [SlashCommand("botinfo", "Get information about the bot.")]
-        public async Task BotInfoCommand(InteractionContext ctx)
-        {
-            var botUser = ctx.Client.CurrentUser;
-            var creationDate = botUser.CreationTimestamp.UtcDateTime;
-            var uptime = DateTime.UtcNow - creationDate;
-            var githubButton = new DiscordLinkButtonComponent("GitHub", "https://github.com/AlienBlox/OgreBotSharp");
-            var installationButton = new DiscordLinkButtonComponent("Install Me!", "https://discord.com/oauth2/authorize?client_id=1508446745816989796&permissions=8&integration_type=0&scope=bot+applications.commands");
+        var botUser = ctx.Client.CurrentUser;
+        var creationDate = botUser.CreationTimestamp.UtcDateTime;
 
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                .WithContent($"🤖 **Bot Information**\n" +
-                             $"**Username:** {botUser.Username}#{botUser.Discriminator}\n" +
-                             $"**ID:** {botUser.Id}\n" +
-                             $"**Created On:** {creationDate:yyyy-MM-dd HH:mm:ss} UTC\n" +
-                             $"**Uptime:** {uptime.Days}d {uptime.Hours}h {uptime.Minutes}m" +
-                             $"**Servers On:** {ctx.Client.Guilds.Count}" +
-                             $"**Shards:** {ctx.Client.ShardCount}" +
-                             "*An open source discord bot made by AlienBlox and the Dragons!*"
-                             ).
-                AddComponents(githubButton, installationButton)
-                );
-        }
+        // Note: This calculates how old the account registration is, not application uptime.
+        var accountAge = DateTime.UtcNow - creationDate;
+
+        // --- FIXED: Swapped URL and Label positions to match API syntax ---
+        var githubButton = new DiscordLinkButtonComponent("https://github.com/AlienBlox/OgreBotSharp", "GitHub");
+        var installationButton = new DiscordLinkButtonComponent("https://discord.com/oauth2/authorize?client_id=1508446745816989796&permissions=8&integration_type=0&scope=bot+applications.commands", "Install Me!");
+
+        // Added clean newlines (\n) to prevent text crowding in the final client layout
+        var responseContent = $"🤖 **Bot Information**\n" +
+                              $"**Username:** {botUser.Username}#{botUser.Discriminator}\n" +
+                              $"**ID:** {botUser.Id}\n" +
+                              $"**Created On:** {creationDate:yyyy-MM-dd HH:mm:ss} UTC\n" +
+                              $"**Account Age:** {accountAge.Days}d {accountAge.Hours}h {accountAge.Minutes}m\n" +
+                              $"**Servers On:** {ctx.Client.Guilds.Count}\n" +
+                              $"**Shards:** {ctx.Client.ShardCount}\n\n" +
+                              $"*An open source discord bot made by AlienBlox and the Dragons!*";
+
+        await ctx.CreateResponseAsync(
+            InteractionResponseType.ChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder()
+                .WithContent(responseContent)
+                .AddComponents(githubButton, installationButton)
+        );
     }
 }
