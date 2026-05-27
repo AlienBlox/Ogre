36b0469716604812220c3007d6a6ea1c3f8188b3
AlienBlox <alienthebest123@outlook.com>
2026-05-27 11:01:37 +0100

    dice and bot info

---
+++ OgreBotSharp/Commands/CoreCommands/BotInfo.cs
@@ -0,0 +1,31 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace OgreBotSharp.Commands.CoreCommands
+{
+    public class BotInfo : ApplicationCommandModule
+    {
+        public async Task BotInfoCommand(InteractionContext ctx)
+        {
+            var botUser = ctx.Client.CurrentUser;
+            var creationDate = botUser.CreationTimestamp.UtcDateTime;
+            var uptime = DateTime.UtcNow - creationDate;
+            var githubButton = new DiscordLinkButtonComponent("GitHub", "https://github.com/AlienBlox/OgreBotSharp");
+            var installationButton = new DiscordLinkButtonComponent("Install Me!", "https://discord.com/oauth2/authorize?client_id=1508446745816989796&permissions=8&integration_type=0&scope=bot+applications.commands");
+
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                .WithContent($"🤖 **Bot Information**\n" +
+                             $"**Username:** {botUser.Username}#{botUser.Discriminator}\n" +
+                             $"**ID:** {botUser.Id}\n" +
+                             $"**Created On:** {creationDate:yyyy-MM-dd HH:mm:ss} UTC\n" +
+                             $"**Uptime:** {uptime.Days}d {uptime.Hours}h {uptime.Minutes}m" +
+                             $"**Servers On:** {ctx.Client.Guilds.Count}" +
+                             $"**Shards:** {ctx.Client.ShardCount}" +
+                             "*An open source discord bot made by AlienBlox and the Dragons!*"
+                             ).
+                AddComponents(githubButton, installationButton)
+                );
+        }
+    }
+}
---
+++ OgreBotSharp/Commands/CoreCommands/Dice.cs
@@ -0,0 +1,16 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace OgreBotSharp.Commands.CoreCommands
+{
+    public class Dice : ApplicationCommandModule
+    {
+        [SlashCommand("dice", "Roll a dice")]
+        public async Task DiceRoll(InteractionContext ctx, [Option("sides", "Number of sides on the dice")] int sides = 6)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                .WithContent($"🎲 You rolled a {new Random().Next(1, sides + 1)} on a {sides}-sided dice!"));
+        }
+    }
+}
