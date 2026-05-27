6bb937dadd8df6d3c4ba900a28202a1cfb0ca42f
AlienBlox <alienthebest123@outlook.com>
2026-05-27 09:34:12 +0100

    test

---
+++ OgreBotSharp/Commands/CoreCommands/ShutdownBot.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace OgreBotSharp.Commands.CoreCommands
+{
+    public class ShutdownBot : ApplicationCommandModule
+    {
+        [SlashCommand("shutdown", "Shuts down the bot.")]
+        public async Task Shutdown(InteractionContext ctx)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
