202d7d53eeb7b0113b68f648c77d09ed12ba1e36
AlienBlox <alienthebest123@outlook.com>
2026-05-27 11:06:32 +0100

    retry

--- OgreBotSharp/Commands/CoreCommands/Dice.cs
+++ OgreBotSharp/Commands/CoreCommands/Dice.cs
@@ -7,10 +7,10 @@ namespace OgreBotSharp.Commands.CoreCommands
     public class Dice : ApplicationCommandModule
     {
         [SlashCommand("dice", "Roll a dice")]
-        public async Task DiceRoll(InteractionContext ctx, [Option("sides", "Number of sides on the dice")] int sides = 6)
+        public async Task DiceRoll(InteractionContext ctx, [Option("sides", "Number of sides on the dice")] long sides = 6)
         {
             await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                .WithContent($"🎲 You rolled a {new Random().Next(1, sides + 1)} on a {sides}-sided dice!"));
+                .WithContent($"🎲 You rolled a {new Random().Next(1, (int)(sides + 1))} on a {sides}-sided dice!"));
         }
     }
 }
