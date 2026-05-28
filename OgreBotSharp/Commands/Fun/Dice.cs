8b2b793ae113de68fb0dda7bda85014c3975f83f
AlienBlox <alienthebest123@outlook.com>
2026-05-28 13:26:23 +0100

    made truly cryptographic

--- OgreBotSharp/Commands/Fun/Dice.cs
+++ OgreBotSharp/Commands/Fun/Dice.cs
@@ -1,16 +1,17 @@
 ﻿using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
+using System.Security.Cryptography;
 
 namespace OgreBotSharp.Commands.Fun
 {
     public class Dice : ApplicationCommandModule
     {
         [SlashCommand("dice", "Roll a dice")]
-        public async Task DiceRoll(InteractionContext ctx, [Option("sides", "Number of sides on the dice")] long sides = 6)
+        public async Task DiceRoll(InteractionContext ctx, [Option("sides", "Number of sides on the dice (With CSPRNG)")] long sides = 6)
         {
             await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                .WithContent($"🎲 You rolled a {new Random().Next(1, (int)(sides + 1))} on a {sides}-sided dice!"));
+                .WithContent($"🎲 You rolled a {RandomNumberGenerator.GetInt32(1, (int)(sides + 1))} on a {sides}-sided dice!"));
         }
     }
 }
