fcce4a075c13f5e4530eeb15818632f41861c45e
AlienBlox <alienthebest123@outlook.com>
2026-05-28 13:31:57 +0100

    toggle between CSPRNG and PRNG

--- OgreBotSharp/Commands/Fun/Dice.cs
+++ OgreBotSharp/Commands/Fun/Dice.cs
@@ -7,9 +7,20 @@ namespace OgreBotSharp.Commands.Fun
 {
     public class Dice : ApplicationCommandModule
     {
-        [SlashCommand("dice", "Roll a dice")]
-        public async Task DiceRoll(InteractionContext ctx, [Option("sides", "Number of sides on the dice (With CSPRNG)")] long sides = 6)
+        [SlashCommand("dice", "Roll a dice (uses CSPRNG by default)")]
+        public async Task DiceRoll(InteractionContext ctx, [Option("sides", "Number of sides on the dice")] long sides = 6, [Option("fast", "Use usual PRNG which is faster but less random")] bool fast = false)
         {
+            if (fast)
+            {
+                var random = new Random();
+
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                    .WithContent($"🎲 You rolled a {random.Next(1, (int)(sides + 1))} on a {sides}-sided dice!"));
+
+                return;
+            }
+
+            
             await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
                 .WithContent($"🎲 You rolled a {RandomNumberGenerator.GetInt32(1, (int)(sides + 1))} on a {sides}-sided dice!"));
         }
